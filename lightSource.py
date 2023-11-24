21b783f0ecb3756d46aaacea7d9b7fbdba2b0518
Kevin <chepstow.kevin@gmail.com>
2023-11-24 14:22:19 +0000

    added code comments for lightsource.py

--- lightSource.py
+++ lightSource.py
@@ -1,29 +1,33 @@
+# imports the library numpy as np
 import numpy as np
 
 class LightSource:
     '''
-    Base class for maintaining a light source in the scene. Inheriting from Sphere allows to visualize the light
-    source position easily.
+    Class for a light source in a 3D scene. It contains properties for ambient, diffuse, 
+    and specular illumination, and can be positioned within the scene.
     '''
     def __init__(self, scene, position=[2.,2.,0.], Ia=[0.2,0.2,0.2], Id=[0.9,0.9,0.9], Is=[1.0,1.0,1.0]):
         '''
-        :param scene: The scene in which the light source exists.
-        :param position: the position of the light source
-        :param Ia: The ambiant illumination it provides (may not be dependent on the light source itself)
-        :param Id: The diffuse illumination
-        :param Is: The specular illumination
-        :param visible: Whether the light should be represented as a sphere in the scene (default: False)
+        Initialise the light source with its properties.
+        :param scene: The scene to which the light belongs.
+        :param position: The 3D position of the light source.
+        :param Ia: Ambient illumination (RGB).
+        :param Id: Diffuse illumination (RGB).
+        :param Is: Specular illumination (RGB).
         '''
 
+        # set the position as a numpy array
         self.position = np.array(position, 'f')
-        self.Ia = Ia
-        self.Id = Id
-        self.Is = Is
+        self.Ia = Ia  # ambient illumination 
+        self.Id = Id  # diffuse illumination
+        self.Is = Is  # specular illumination
 
     def update(self, position=None):
         '''
-        update the position of the light source.
-        :param position: [optional] sets the current light source position.
+        Update the position of the light source
+        :param position: New position for the light source
         '''
+
+        # update position if provided
         if position is not None:
             self.position = position
